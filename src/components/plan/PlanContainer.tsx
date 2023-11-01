c3d20bb268e89ad3a5ddf85c70a1e5bd282de52a
Nate Wiltzius <novawiltzius@gmail.com>
2023-11-01 00:39:15 -0500

    Reworking trip length

--- src/components/plan/PlanContainer.tsx
+++ src/components/plan/PlanContainer.tsx
@@ -4,33 +4,59 @@ import CardContent from '@mui/material/CardContent';
 import React from 'react';
 import { typeToChunkMap } from '@/constants/maps';
 import { Plan } from '@/types/plan.types';
+import { Box, Divider } from '@mui/material';
 
 type Props = {
   plan: Plan;
 };
 
-const PlanContainer = ({ plan }: Props) => (
-  <Card className="flex flex-col w-1/2 h-fit">
-    <CardContent className="flex flex-row justify-around">
-      <h2>
-        <b>Your Plan</b>
-      </h2>
-      <Button variant="contained" type="submit" className="w-fit bg-primary">
-        Save
-      </Button>
-    </CardContent>
-    <CardContent>
-      {plan.chunks && plan.chunks.length > 0 && (
-        <div className="flex flex-col gap-4">
-          {plan.chunks.map((chunk, i) => {
-            const ChunkComponent = typeToChunkMap.get(chunk.type);
-            if (!ChunkComponent) return null;
-            return <ChunkComponent key={i} chunkData={chunk.data} />;
-          })}
-        </div>
+const PlanContainer = ({ plan }: Props) => {
+  const numberOfDays = parseInt(
+    plan.configs
+      .find((config) => config.type === 'Trip Length')
+      ?.data.split(' ')[0] ?? '0',
+    10,
+  );
+
+  return (
+    <Card className="flex flex-col w-1/2 h-fit">
+      <CardContent className="flex flex-row justify-around">
+        <h2>
+          <b>Your Plan</b>
+        </h2>
+        <Button variant="contained" type="submit" className="w-fit bg-primary">
+          Save
+        </Button>
+      </CardContent>
+      {/* <CardContent>
+        {plan.chunks && plan.chunks.length > 0 && (
+          <div className="flex flex-col gap-4">
+            {plan.chunks.map((chunk, i) => {
+              const ChunkComponent = typeToChunkMap.get(chunk.type);
+              if (!ChunkComponent) return null;
+              return <ChunkComponent key={i} chunkData={chunk.data} />;
+            })}
+          </div>
+        )}
+      </CardContent> */}
+      {numberOfDays > 0 && (
+        <CardContent className="flex flex-col justify-around gap-2">
+          {Array.from({ length: numberOfDays }, (_, i) => i + 1).map((day) => (
+            <div key={day} className="w-full">
+              <div>Day {day}</div>
+              <Divider />
+              <Box height={40}></Box>
+            </div>
+          ))}
+        </CardContent>
       )}
-    </CardContent>
-  </Card>
-);
+      <CardContent className="flex flex-row justify-around">
+        <Button variant="contained" type="submit" className="w-fit bg-primary">
+          Add Day
+        </Button>
+      </CardContent>
+    </Card>
+  );
+};
 
 export default PlanContainer;
--- src/components/plan/forms/configForms/TripLengthForm.tsx
+++ src/components/plan/forms/configForms/TripLengthForm.tsx
@@ -1,59 +1,37 @@
 import React, { FormEvent, useCallback, useState } from 'react';
 import Form from '@/components/shared/Form';
 import TextField from '@mui/material/TextField';
-import MenuItem from '@mui/material/MenuItem';
 import useConfigForm from '@/hooks/useConfigForm';
-
-const units = [
-  { label: 'Days', value: 'Days' },
-  { label: 'Weeks', value: 'Weeks' },
-  { label: 'Months', value: 'Months' },
-  { label: 'Years', value: 'Years' },
-];
+import { InputAdornment } from '@mui/material';
 
 const FORM_KEY = 'Trip Length';
 
 const TripLengthForm = () => {
   const { planData, handleSubmit } = useConfigForm(FORM_KEY);
-  const [unit, setUnit] = useState<string>(
-    planData?.configs
-      ?.find((config) => config.type === FORM_KEY)
-      ?.data.split(' - ')[0] || 'Days',
-  );
   const [length, setLength] = useState(
     planData?.configs
       ?.find((config) => config.type === FORM_KEY)
-      ?.data.split(' - ')[1] || '0',
+      ?.data.split(' ')[0] || '0',
   );
 
   const handleTripLengthSubmit = useCallback(
     (e: FormEvent<HTMLFormElement>) => {
       e.preventDefault();
-      handleSubmit(e, `${unit} - ${length}`);
+      handleSubmit(e, `${length} days`);
     },
-    [handleSubmit, length, unit],
+    [handleSubmit, length],
   );
 
   return (
     <Form onSubmit={handleTripLengthSubmit}>
       <TextField
-        id="unit-select"
-        select
-        value={unit}
-        label="Unit"
-        onChange={(e) => setUnit(e.target.value)}
-      >
-        {units.map((u) => (
-          <MenuItem key={u.value} value={u.value}>
-            {u.label}
-          </MenuItem>
-        ))}
-      </TextField>
-      <TextField
         type="number"
-        placeholder="Trip Length"
+        placeholder="How many days?"
         label="Trip Length"
         value={length}
+        InputProps={{
+          endAdornment: <InputAdornment position="end">days</InputAdornment>,
+        }}
         onChange={(e) => setLength(e.target.value)}
       />
     </Form>
