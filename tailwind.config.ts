6952a7600cc28fe88bbde8bebc4c4197c6bdab3a
Nate Wiltzius <novawiltzius@gmail.com>
2025-01-27 01:49:39 -0700

    Added MUI and fixed tailwind and eslint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,25 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  important: '#__next',
   theme: {
+    fontFamily: {
+      sans: ['var(--font-roboto)', 'ui-sans-serif', 'system-ui'],
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
   plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
 } satisfies Config;
